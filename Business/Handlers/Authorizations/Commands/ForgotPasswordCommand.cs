b1b600777d821a85908f9fa4c7f20b6e5603e66e
Cemil Şamlı <samlicemil.m@gmail.com>
2025-07-02 11:30:25 +0300

    initial

---
+++ Business/Handlers/Authorizations/Commands/ForgotPasswordCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Business.BusinessAspects;
+using Business.Constants;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Logging;
+using Core.CrossCuttingConcerns.Logging.Serilog.Loggers;
+using Core.Utilities.Mail;
+using Core.Utilities.Results;
+using Core.Utilities.Security.Hashing;
+using Core.Utilities.Toolkit;
+using DataAccess.Abstract;
+using MediatR;
+
+namespace Business.Handlers.Authorizations.Commands
+{
+    public class ForgotPasswordCommand : IRequest<IResult>
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+
+        public class ForgotPasswordCommandHandler : IRequestHandler<ForgotPasswordCommand, IResult>
+        {
+            private readonly IUserRepository _userRepository;
+            private readonly IMailService _mailService;
+
+			public ForgotPasswordCommandHandler(IUserRepository userRepository, IMailService mailService)
+			{
+				_userRepository = userRepository;
+				_mailService = mailService;
+			}
+
+			/// <summary>
+			/// </summary>
+			/// <param name="request"></param>
+			/// <param name="cancellationToken"></param>
+			/// <returns></returns>
+			[SecuredOperation(Priority = 1)]
+            [CacheRemoveAspect()]
+            [LogAspect(typeof(FileLogger))]
+            public async Task<IResult> Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
+            {
+                var user = await _userRepository.GetAsync(u => u.UserId == Convert.ToInt64(request.UserId));
+
+                if (user == null)
+                {
+                    return new ErrorResult(Messages.WrongCitizenId);
+                }
+
+                var generatedPassword = RandomPassword.CreateRandomPassword(14);
+                HashingHelper.CreatePasswordHash(generatedPassword, out var passwordSalt, out var passwordHash);
+
+
+				_userRepository.Update(user);
+
+                return new SuccessResult(Messages.SendPassword + Messages.NewPassword + generatedPassword);
+            }
+        }
+    }
+}
---
+++ Core/Utilities/Messages/SwaggerMessages.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Utilities.Messages
+{
+    public static class SwaggerMessages
+    {
+        public static string Version => "v1";
+        public static string Title => "Mam Yazılım";
+        public static string TermsOfService => "";
+        public static string ContactName => "Mam Yazılım";
+        public static string LicenceName => "Use under LICX";
+        public static string ContactEMail => "Use under LICX";
+        public static string ContactUrl => "";
+        public static string LicenceUrl => "";
+
+        public static string Description => @"";
+    }
+}
