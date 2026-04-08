5c27faed384bbafedc75e9424af6ac1663c42ff2
Veli HASAR <vhas_ar@hotmail.com>
2026-04-08 23:04:03 +0300

    SinavBolumAdded baseEnt mig

---
+++ DataAccess/Migrations/Pg/20260408200020_SinavBolumaddedBase.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations.Pg
+{
+    /// <inheritdoc />
+    public partial class SinavBolumaddedBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedBy",
+                table: "SinavBolums",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "SinavBolums",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "SinavBolums",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdatedBy",
+                table: "SinavBolums",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "SinavBolums",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "SinavBolums");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "SinavBolums");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "SinavBolums");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "SinavBolums");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "SinavBolums");
+        }
+    }
+}
